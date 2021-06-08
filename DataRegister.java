212bad55d53b6a5da055750d20dc43df5dde9fa9
unknown <julio.gonzalez7589@alumnos.udg.mx>
2021-06-08 17:07:14 -0500

    Fix tiny error

--- DataRegister.java
+++ DataRegister.java
@@ -3,12 +3,12 @@ package examples.mlr;
 public class DataRegister {
     private double x;
     private double x2;
-    private double z;
+    private double y;
 
-    public DataRegister(double x, double x2, double z) {
+    public DataRegister(double x, double x2, double y) {
         this.x = x;
         this.x2 = x2;
-        this.z = z;
+        this.y = y;
     }
 
     public double getX1() {
@@ -28,11 +28,11 @@ public class DataRegister {
     }
 
     public double getY() {
-        return this.z;
+        return this.y;
     }
 
-    public void setY(double z) {
-        this.z = z;
+    public void setY(double y) {
+        this.y = y;
     }
 }
 
